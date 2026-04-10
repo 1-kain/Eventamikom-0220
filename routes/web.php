b73bf095f08db18c88195195bf4f9fa15cc1e594
1-kain <kalvin.dreamer@gmail.com>
2026-04-10 09:37:23 +0700

    Tugas 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tentang', function () {
+    return '<h1>Ini adalah halaman tentang aplikasi event hub</h1>';
+});
+
+Route::get('/kontak', function () {
+    return view('contact');
+});
