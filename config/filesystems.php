5ed6143e23405bb8c66f20c3ca5d32f54dbb4378
1-kain <kalvin.dreamer@gmail.com>
2026-06-19 20:42:54 +0700

    pembenahan upload image di infinity free

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => $_SERVER['DOCUMENT_ROOT'] . '/storage',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
